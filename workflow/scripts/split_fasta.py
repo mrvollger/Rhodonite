2c1051a3a6ea3211e594279b8943fd6315c93cc6
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-07-26 08:46:41 -0700

    adding max length

--- workflow/scripts/split_fasta.py
+++ workflow/scripts/split_fasta.py
@@ -16,15 +16,42 @@ if __name__ == "__main__":
     parser.add_argument(
         "-w", "--width", help="width of fasta output", type=int, default=60
     )
+    parser.add_argument(
+        "-r",
+        "--reheader",
+        help="Make new fasta names and save them in this file",
+        default=None,
+    )
+    parser.add_argument(
+        "-m",
+        "--maxheader",
+        help="Set a maximum header length for the input fasta",
+        default=None,
+        type=int,
+    )
+
     args = parser.parse_args()
     N_IDS = len(args.outputs)
 
+    if args.reheader is not None:
+        conversion = open(args.reheader, "w+")
     fasta = pysam.FastaFile(args.infile)
 
     outs = [open(f, "w+") for f in args.outputs]
     out_idx = 0
-    for name in fasta.references:
+    for idx, name in enumerate(fasta.references):
         seq = fasta.fetch(name)
+
+        if args.reheader is not None:
+            new_name = f"RM_NAME_{idx}"
+            conversion.write(f"{new_name}\t{name}\n")
+            name = new_name
+
+        if args.maxheader is not None and len(name) > args.maxheader:
+            raise IOError(
+                f"fasta header {name} is longer than {args.maxheader}. Please change your headers to be shorter than {args.maxheader} characters. (https://github.com/rmhubley/RepeatMasker/issues/59)"
+            )
+
         seq_fold = "\n".join(textwrap.wrap(seq, args.width)).strip()
         outs[out_idx].write(">{}\n{}\n".format(name, seq_fold))
         out_idx += 1
